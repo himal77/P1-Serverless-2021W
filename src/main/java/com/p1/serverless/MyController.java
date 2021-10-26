6c78bd76dbbe8fbe47547e51945801b8d601a2e5
Himal <purihimal9@gmail.com>
2021-10-26 21:56:52 +0200

    successfully took image via post mapping

--- src/main/java/com/p1/serverless/MyController.java
+++ src/main/java/com/p1/serverless/MyController.java
@@ -11,39 +11,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RestController
 public class MyController {
 
-    @GetMapping(value = "/test1", produces = MediaType.IMAGE_JPEG_VALUE)
-    public void thumbnailImage(HttpServletResponse response) throws IOException {
-        ClassPathResource file = new ClassPathResource("testimage.jpeg");
-        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
-        StreamUtils.copy(file.getInputStream(), response.getOutputStream());
-    }
-
-    @GetMapping(value = "/sid2", produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<byte[]> get_thumbnailImage() throws IOException {
-        ClassPathResource file = new ClassPathResource("testimage.jpeg");
+    @PostMapping(value = "/test1", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> uploadFile(@RequestParam MultipartFile file, HttpServletResponse response) throws IOException {
         byte[] bytes = StreamUtils.copyToByteArray(file.getInputStream());
+        System.out.println(file.getName());
         return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(bytes);
-    }
-
-    @GetMapping(value = "/sid3", produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<InputStreamResource> get_thumbnailImage_resource() throws IOException {
-        ClassPathResource file = new ClassPathResource("testimage.jpeg");
-        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG)
-                .body(new InputStreamResource(file.getInputStream()));
-    }
-
-    @PostMapping(value = "/file")
-    public void uploadFile(@RequestParam MultipartFile file, HttpServletResponse response) throws IOException {
-        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
-        StreamUtils.copy(file.getInputStream(), response.getOutputStream());
-
-        // Everything was fine, you should give name as file in postman key.
+        // there should be always produces MediaType.IMAGE_JPEG_VALUE
     }
 }
